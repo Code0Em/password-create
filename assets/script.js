bdf0338ac786c1e707ade457787e01dff4da906e
code0em <146350167+Code0Em@users.noreply.github.com>
2023-12-16 11:33:03 +0000

    added pseudocode for task 1

--- assets/script.js
+++ assets/script.js
@@ -88,15 +88,19 @@ const upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+// **TASK 1: Present a series of prompts for password criteria, prompting for length of password (at least 8 characters but no more than 128) and character types (lowercase, uppercase, numeric, and special characters).
 
-}
+// *TASK 1.1: Create object to store user's password options.
 
-// Function for getting a random element from an array
-function getRandom(arr) {
 
-}
+// *TASK 1.2: Create function to prompt the user for password options.
+// *TASK 1.2.1: Use confirm length to ask questions with yes/no answers (i.e. questions about password characters).
+// WORKINGS: Character type questions all require a yes or no answer (i.e. true or false). Confirm method returns true (if user selects OK) or otherwise returns false, so we can use this method to ask these questions.
+
+// *TASK 1.2.2: Use prompt method to ask question which requires user input in answer (i.e. question about password length).
+// *TASK 1.2.3: Use if/else statement and do/while loop to validate user’s input (i.e. check the answer is a number between 8 and 128, and keep asking if not).
+// WORKINGS: Length question requires a number as the answer (i.e. user input). Prompt method returns user’s input value (if user selects OK) otherwise it returns null, so we can use this method to ask the length question. However, we need to validate this to ensure a number (within our specified range) is inputted by the user. If/else statement(s) check condition(s) and only run the code block if the condition(s) are met, so we can use this to set our conditions (i.e. length must be between 8 and 128, and cannot not be a number). But we then need to keep asking this question until the user’s input meets our conditions. We can use a do/while loop for this. Do/while loops will execute the code block once (i.e. ask the question once) before checking if the condition is true (i.e. does the answer meet our conditions) and if not, will loop through again (i.e. keep asking the question) and so on, until the condition(s) are met (i.e. until the user inputs a number between 8 and 128).
+
 
 // Function to generate password with user input
 function generatePassword() {
